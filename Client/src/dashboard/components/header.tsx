dd0de7dbbd4e1f0c4842b43a8bb82961e4b3ec3e
Thisuru Dineth <thisurudineth40@gmail.com>
2024-10-18 10:28:25 +0530

    Dashboard responsive

--- Client/src/dashboard/components/header.tsx
+++ Client/src/dashboard/components/header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
     return (
         <header className="border-b">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <h1 className="text-xl font-semibold">Rysera - 3D Printing</h1>
+          <h1 className="text-base font-semibold lg:text-xl">Rysera - 3D Printing</h1>
           <div className="flex items-center gap-4">
             <button className="hover:text-gray-900 bg-neutral-200">View Orders</button>
             <Avatar>
--- Client/src/dashboard/dashboard.tsx
+++ Client/src/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 import { PlusIcon, TrashIcon, UploadCloud } from "lucide-react";
 import {
   Select,
@@ -8,11 +8,14 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Card, CardContent } from "@/components/ui/card";
-import PLA from "../assets/pla.png";
+import PLA from "../assets/PLA.png";
+import ABS from "../assets/ABS.webp";
+import PETG from "../assets/PETG.webp";
+import TPU from "../assets/TPU.png";
 import STLViewer from "./components/STLViewer";
 import { Button } from "@/components/ui/button";
 import Estimator from "./components/estimator";
-
+import axios from 'axios';
 import {
   Pagination,
   PaginationContent,
@@ -21,6 +24,13 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+
+const materialImages: { [key: string]: string } = {
+  PLA: PLA,
+  ABS: ABS,
+  PETG: PETG,
+  TPU: TPU,
+};
 
 export default function Dashboard() {
   const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
@@ -78,28 +88,68 @@ export default function Dashboard() {
       setEstimatedPrices((prevPrices) => ({ ...prevPrices, [index]: price }));
     }, 2000);
   };
+  
+  interface Material {
+    name: string;
+    description: string;
+    properties: {
+      Tensile: string;
+      elongation: string;
+      Flexural: string;
+      HDT: string;
+    };
+    colors: string[];
+  }
+
+  const [materials, setMaterials] = useState<Material[]>([]);
+  const [selectedMaterial, setSelectedMaterial] = useState<Material | null>(null);
+
+  useEffect(() => {
+    const fetchMaterials = async () => {
+      const response = await axios.get('/materials.json');
+      setMaterials(response.data);
+      const defaultMaterial = response.data.find((mat: Material) => mat.name === "PLA");
+      setSelectedMaterial(defaultMaterial);
+    };
+
+    fetchMaterials();
+  }, []);
+
+  const handleMaterialChange = (e: string) => {
+    const material = materials.find(mat => mat.name === e);
+    if (material) {
+      setSelectedMaterial(material);
+    }
+  };
+
 
   return (
-    <div className="flex gap-6 h-[85vh]">
+    <div className="flex gap-4 h-[80vh]">
       {/* PLA Info Card (Left) */}
-      <Card className="p-6 w-1/4">
-        <div className="mb-6">
-          <img
-            src={PLA}
-            alt="PLA Filament"
+      <Card className="p-6 w-1/5 hidden lg:block">
+        <div className="mb-2">
+        <img
+            src={selectedMaterial ? materialImages[selectedMaterial.name] : PLA}
+            alt={selectedMaterial ? `${selectedMaterial.name} Filament` : "Material"}
             className="w-full h-auto rounded-lg"
-          />
+        />
         </div>
         <CardContent className="p-0">
-          <p className="text-gray-600">
-            PLA (Polylactic Acid) is an excellent 3D printing material because
-            it's biodegradable, easy to print with, and produces high-quality,
-            precise prints with minimal warping or odor.
-          </p>
+        {selectedMaterial && (
+        <div>
+          <p className="font-semibold">{selectedMaterial.name}</p>
+          <p className="text-sm mb-3">{selectedMaterial.description}</p>
+          <p className="text-sm"><strong>Tensile strength:</strong> {selectedMaterial.properties.Tensile}</p>
+          <p className="text-sm"><strong>Tensile elongation:</strong> {selectedMaterial.properties.elongation}</p>
+          <p className="text-sm"><strong>Flexural strength:</strong> {selectedMaterial.properties.Flexural}</p>
+          <p className="text-sm"><strong>HDT (0.45 MPa):</strong> {selectedMaterial.properties.HDT}</p>
+        </div>
+      )}
         </CardContent>
       </Card>
 
       {/* Upload Area or STL Viewer (Center) */}
+      <div className="flex flex-row flex-grow flex-wrap justify-center lg:flex-nowrap">
       <div
         className={
           "flex flex-col items-center justify-center w-full col-span-1 border rounded-lg relative"
@@ -177,7 +227,7 @@ export default function Dashboard() {
           </>
         ) : (
           <div
-            className={`flex flex-col items-center justify-center border-2 border-dashed p-16 rounded-lg ${
+            className={`flex flex-col items-center justify-center border-2 border-dashed p-8 lg:p-16 rounded-lg ${
               dragActive ? "border-blue-500" : "border-gray-200"
             }`}
           >
@@ -197,45 +247,48 @@ export default function Dashboard() {
                 </label>
               </p>
               <p className="text-sm text-gray-500 mt-2">
-                .STL files up to 200mb
+                .STL files up to 50mb
               </p>
             </div>
           </div>
-        )}
-      </div>
+          )}
+        </div>
+      
 
-      {/* Settings Panel (Right) */}
-      <div className="w-1/4">
-        <div className="space-y-6">
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Quality
-            </label>
-            <Select defaultValue="0.12">
-              <SelectTrigger>
-                <SelectValue placeholder="Select quality" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="0.12">0.12 - fine</SelectItem>
-                <SelectItem value="0.16">0.16 - medium</SelectItem>
-                <SelectItem value="0.20">0.20 - draft</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-
+        <div className="w-full space-y-2 pl-16 pr-16 pt-8 ml-0 lg:pl-2 lg:pr-2 lg:pt-0 lg:ml-4 lg:w-1/4">
           {/* Material Type Selector */}
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-2">
               Material type
             </label>
-            <Select defaultValue="pla">
+            <Select defaultValue="PLA" onValueChange={handleMaterialChange}>
+            <SelectTrigger>
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              {materials.map(material => (
+                <SelectItem key={material.name} value={material.name}>
+                  {material.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+            </Select>
+          </div>
+          
+          {/* Settings Panel (Right) */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Quality
+            </label>
+            <Select defaultValue="0.20">
               <SelectTrigger>
-                <SelectValue placeholder="Select material" />
+                <SelectValue placeholder="Select quality" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="pla">PLA</SelectItem>
-                <SelectItem value="abs">ABS</SelectItem>
-                <SelectItem value="petg">PETG</SelectItem>
+                <SelectItem value="0.12">0.12 - fine</SelectItem>
+                <SelectItem value="0.16">0.16 - optimal</SelectItem>
+                <SelectItem value="0.20">0.20 - standard</SelectItem>
+                <SelectItem value="0.24">0.24 - draft</SelectItem>
               </SelectContent>
             </Select>
           </div>
@@ -245,14 +298,23 @@ export default function Dashboard() {
             <label className="block text-sm font-medium text-gray-700 mb-2">
               Color
             </label>
-            <Select defaultValue="white">
+            <Select>
               <SelectTrigger>
                 <SelectValue placeholder="Select color" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="white">White</SelectItem>
-                <SelectItem value="black">Black</SelectItem>
-                <SelectItem value="gray">Gray</SelectItem>
+              {selectedMaterial && (() => {
+                const items = [];
+                for (let i = 0; i < selectedMaterial.colors.length; i++) {
+                  const color = selectedMaterial.colors[i];
+                  items.push(
+                    <SelectItem key={color.toLowerCase()} value={color.toLowerCase()}>
+                      {color}
+                    </SelectItem>
+                  );
+                }
+                return items;
+              })()}
               </SelectContent>
             </Select>
           </div>
